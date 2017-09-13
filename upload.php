b45af46abc894826e86d91045bbe6cf2aeeba58a
Hector Rodriguez <hecrodfig@gmail.com>
2017-09-13 16:26:51 -0400

    Update on the upload.php

--- upload.php
+++ upload.php
@@ -55,38 +55,96 @@ if(isset($_GET)){
 $allDataInSheet = $objPHPExcel->getActiveSheet()->toArray( null, true, true, true );
 
 $arrayCount = count( $allDataInSheet ); // Here get total count of row in that Excel sheet
-
-
-
-for ( $i = 1; $i <= $arrayCount; $i++ ) {
+ 
+for ( $i = 2; $i <= $arrayCount; $i++ ) {
 
 	$year = trim( $allDataInSheet[ $i ][ "A" ] );
 
-	$name = trim( $allDataInSheet[ $i ][ "B" ] );
+	$rnd = trim( $allDataInSheet[ $i ][ "B" ] );
+	
+	$dt = trim( $allDataInSheet[ $i ][ "C" ] );
 
-	$query = "SELECT bb_year FROM records WHERE bb_year = '" . $year . "' and bb_rnd = '" . $name . "'";
+	$ovpck = trim( $allDataInSheet[ $i ][ "D" ] );
+	
+	$frrnd = trim( $allDataInSheet[ $i ][ "E" ] );
+
+	$rdpck = trim( $allDataInSheet[ $i ][ "F" ] );
+	
+	$tm = trim( $allDataInSheet[ $i ][ "G" ] );
+
+	$name = trim( $allDataInSheet[ $i ][ "H" ] );
+	
+	$pos = trim( $allDataInSheet[ $i ][ "I" ] );
+
+	$war = trim( $allDataInSheet[ $i ][ "J" ] );
+	
+	$g = trim( $allDataInSheet[ $i ][ "K" ] );
+
+	$ab = trim( $allDataInSheet[ $i ][ "L" ] );
+	
+	$hr = trim( $allDataInSheet[ $i ][ "M" ] );
+
+	$ba = trim( $allDataInSheet[ $i ][ "N" ] );
+	
+	$ops = trim( $allDataInSheet[ $i ][ "O" ] );
+
+	$g2 = trim( $allDataInSheet[ $i ][ "P" ] );
+	
+	$w = trim( $allDataInSheet[ $i ][ "Q" ] );
+
+	$l = trim( $allDataInSheet[ $i ][ "R" ] );
+	
+	$era = trim( $allDataInSheet[ $i ][ "S" ] );
+
+	$whip = trim( $allDataInSheet[ $i ][ "T" ] );
+	
+	$sv = trim( $allDataInSheet[ $i ][ "U" ] );
+
+	$type = trim( $allDataInSheet[ $i ][ "V" ] );
+	
+	$doo = trim( $allDataInSheet[ $i ][ "W" ] );
+
+	$state = trim( $allDataInSheet[ $i ][ "X" ] );
+	
+	$signed = trim( $allDataInSheet[ $i ][ "Y" ] );
+
+	$pv = trim( $allDataInSheet[ $i ][ "Z" ] );
+	
+	$bonus = trim( $allDataInSheet[ $i ][ "AA" ] );
+
+	$diff = trim( $allDataInSheet[ $i ][ "AB" ] );
+	
+	$bonus_per = trim( $allDataInSheet[ $i ][ "AC" ] );
+
+//Verify that they are not records with the same content
+	$query = "SELECT bb_year FROM records WHERE bb_year = '" . $year . "' and bb_rnd = '" . $rnd . "'";
 
 	$sql = mysqli_query( $connection, $query );
 	
-	$recResult = mysqli_fetch_array( $sql );
+	$recResult = mysqli_fetch_array($sql);
 
 	$existName = $recResult[ "bb_year" ];
 
+//If $existName is empty it proceed with the INSERT query below 
+
 	if ( $existName == "" ) {
-		$query = "insert into records (bb_year, bb_rnd) values('" . $year . "', '" . $name . "');";
+		$query = "insert into records (bb_year, bb_rnd, bb_dt, bb_ovpck, bb_frrnd, bb_rdpck, bb_tm, bb_name, bb_pos, bb_war, bb_g, bb_ab, bb_hr, bb_ba, bb_ops, bb_g2, bb_w, bb_l, bb_era, bb_whip, bb_sv, bb_type, bb_doo, bb_state, bb_signed, bb_pv, bb_bonus, bb_diff, bb_bonus_per, bb_when) values('" . $year . "', '" . $rnd . "', '" . $dt . "', '" . $ovpck . "', '" . $frrnd . "', '" . $rdpck . "', '" . $tm . "', '" . $name . "', '" . $pos . "', '" . $war . "', '" . $g . "', '" . $ab . "', '" . $hr . "', '" . $ba . "', '" . $ops . "', '" . $g2 . "', '" . $w . "', '" . $l . "', '" . $era . "', '" . $whip . "', '" . $sv . "', '" . $type . "', '" . $doo . "', '" . $state . "', '" . $signed . "', '" . $pv . "', '" . $bonus . "', '" . $diff . "', '" . $bonus_per . "', 'NOW()');";
 		$insertTable = mysqli_query( $connection, $query );
+		
 
-
-		$msg = 'Record has been added. <div style="Padding:20px 0 0 0;"><a href="index.php" target="_blank">Go Back to tutorial</a></div>';
+//Messages of success in a variable
+		
+		$msg = 'Record has been added. <div style="Padding:20px 0 0 0;"><a href="index.php" target="_self">Go Back to tutorial</a></div>';
 
 	} else {
 
-		$msg = 'Record already exist. <div style="Padding:20px 0 0 0;"><a href="index.php" target="_blank">Go Back to tutorial</a></div>';
+		$msg = 'Record already exist. <div style="Padding:20px 0 0 0;"><a href="index.php" target="_self">Go Back to tutorial</a></div>';
 
 	}
 
 }
 
+//Echo of the variable of success 
 echo "<div style='font: bold 18px arial,verdana;padding: 45px 0 0 500px;'>" . $msg . "</div>";
 
 ?>
